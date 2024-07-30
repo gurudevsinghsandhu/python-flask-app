570020235594460543d176d370b9562f427af6e3
Gurudev singh <144236322+gurudevsinghsandhu@users.noreply.github.com>
2024-07-30 17:54:15 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker push gurudevsingh/simple-python-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 gurudevsingh/simple-python-app:latest
